1f6f65e6676f5d4a25fbfe9656796a58080de837
Erick Chung <echung428@gmail.com>
2025-09-20 17:52:36 -0400

    9/20

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { auth, signIn, signOut } from "@/auth";
 
+
 // Note how we can turn the entire component into async cuz its a server side component
 const Navbar = async () => {
   // How to know if user is logged in? Look into a user session that comes directly from Next Auth
@@ -14,27 +15,33 @@ const Navbar = async () => {
         </Link>
 
         <div className="flex items-center gap-5 text-black">
-          {session && session?.user ? (
+          {session?.user ? ( // If user is logged in, show these buttons (if session exists and if that session has a user)
+          //If session exists, it means the user is logged in and auth systems guarantee that user exists. if not logged in, session is set to null
             <>
               <Link href="/startup/create">
                 <span>Create</span>
               </Link>
-              <button onClick={signOut}>
-                <span>Logout</span>
-              </button>
+              <form action={async () => {
+                "use server";
+                await signOut({ redirectTo: "/"}); // Adding the reDirectTo makes it so that after you sign out, you are redirected to home page
+              }}>
+                <button type="submit">Logout</button>
+              </form>
 
               <Link href={`/user/${session?.id}`}>
                 <span>{session?.user?.name}</span>
               </Link>
             </>
           ) : (
-            <button
-              onClick={() => {
+            // Use a form with a server action to run server code. You cannot do this with buttons and onClick functions
+            <form
+              action={async () => {
+                "use server";
                 await signIn("github");
               }}
             >
-              <span>Login</span>
-            </button>
+              <button type="submit">Login</button>
+            </form>
           )}
         </div>
       </nav>
--- auth.ts
+++ auth.ts
@@ -4,3 +4,8 @@ import GitHub from "next-auth/providers/github";
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
 });
+
+
+// NextAuth() returns an object that includes handlers and the auth,signIn, and signOut functions
+
+// We will use destructuring to pull those functions out of the NextAuth object and make them individual variables
